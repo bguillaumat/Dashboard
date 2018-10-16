8137a511e8049fe8d11b2ed7894b0460d08af31d
bguillaumat <bastien.guillaumat@epitech.eu>
2018-10-16 11:54:24 +0200

    Login Page

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 var express = require('express');
+var request = require('request');
 var app = express();
 
-app.get('/test/', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.send('Vous êtes à l\'accueil');
+var openweathermeteo = function(city, callback){
+    var  url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+city+'&cnt=1&mode=json&units=metric&lang=fr&appid=521c8f8246c012c8421856de66e06c2a';
+
+    request(url, function(err, response, body){
+        try{
+            var result = JSON.parse(body);
+            var previsions = {
+                temperature : result.list[0].temp.day,
+                city : result.city.name,
+                state : result.list[0].weather[0].main
+            };
+
+            callback(null, previsions);
+        }catch(e){
+            callback(e);
+        }
+    });
+}
+
+app.get('/', function(req, res) {
+    res.render("log.ejs");
 });
 
 app.listen(8080);
