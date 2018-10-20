a6ae936f630a953f3176aa51f7104ba3ade9ad0e
bguillaumat <bastien.guillaumat@epitech.eu>
2018-10-20 13:04:39 +0200

    Steam modifications

--- scripts/steam.js
+++ scripts/steam.js
@@ -1,21 +1,14 @@
 let request = require('request');
 
 exports.steamName = function(id, callback) {
-    let  url = 'https://api.steampowered.com/ISteamApps/GetAppList/v1/';
+    let url = 'https://store.steampowered.com/api/appdetails?appids='+id;
 
     request(url, function(err, response, body){
         try{
             let result = JSON.parse(body);
             if (result == null)
                 return null;
-            let x = 0;
-            while (result.applist.apps.app[x]) {
-                if (result.applist.apps.app[x].appid == id) {
-                    callback(null, result.applist.apps.app[x].name);
-                    break;
-                }
-                x += 1;
-            }
+            callback(null, Object.values(result)[0].data.name);
             callback(null, null);
         }catch(e){
             callback(e);
