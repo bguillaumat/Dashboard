f3f20c1018996cd3669da5243448a81019aa9fcd
bguillaumat <bastien.guillaumat@epitech.eu>
2018-10-21 23:25:52 +0200

    Reddit bug fix

--- scripts/reddit.js
+++ scripts/reddit.js
@@ -5,14 +5,27 @@ exports.getLastSub = function(sub, nbr, callback){
 
     request(url, function(err, response, body){
         try{
-            let result = JSON.parse(body);
-            let posts = [];
-            for (let i = 0; i < nbr; ++i) {
-                if (result.data.children[i].data.title != null) {
-                    posts.push({title: result.data.children[i].data.title, thumbnail: result.data.children[i].data.thumbnail, author: result.data.children[i].data.author, link: result.data.children[i].data.permalink});
+            if (body.startsWith("<"))
+                callback(null, null);
+            else {
+                let result = JSON.parse(body);
+                if (result.error.toString() === "404")
+                    callback(null, null);
+                else {
+                    let posts = [];
+                    for (let i = 0; i < nbr; ++i) {
+                        if (result.data.children[i].data.title != null) {
+                            posts.push({
+                                title: result.data.children[i].data.title,
+                                thumbnail: result.data.children[i].data.thumbnail,
+                                author: result.data.children[i].data.author,
+                                link: result.data.children[i].data.permalink
+                            });
+                        }
+                    }
                 }
+                callback(null, posts);
             }
-            callback(null, posts);
         }catch(e){
             callback(e);
         }
